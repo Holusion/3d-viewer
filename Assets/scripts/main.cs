5fa7c2d104763403034c56f54386c4edfde9c250
Sebastien Dumetz <s.dumetz@holusion.com>
2014-10-27 11:41:45 +0100

    resolved libraries problems

---
+++ Assets/scripts/main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class NewBehaviourScript : MonoBehaviour {
+	Controller controller;
+	// Use this for initialization
+	void Start () {
+		controller = new Controller();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame();
+	}
+}
