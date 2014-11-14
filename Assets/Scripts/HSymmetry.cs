80d9d9158c60152c2fb2c37ad5af82020aff2fe9
Sebastien Dumetz <s.dumetz@holusion.com>
2014-11-14 16:30:26 +0100

    added an horizontal symmetry script for cameras

---
+++ Assets/Scripts/HSymmetry.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Camera))]
+public class HSymmetry : MonoBehaviour {
+
+	void OnPreCull () {
+		Matrix4x4 scale = Matrix4x4.Scale (new Vector3 (-1, 1, 1));
+
+		camera.ResetWorldToCameraMatrix ();
+		camera.ResetProjectionMatrix ();
+		camera.projectionMatrix = camera.projectionMatrix * scale;
+	}
+	void OnPreRender () {
+		GL.SetRevertBackfacing (true);
+	}
+	void OnPostRender () {
+		GL.SetRevertBackfacing (false);
+	}
+}
+
+
+
+
