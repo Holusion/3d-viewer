bd6a7eeb056e0b7ecdc21a2e6dfe580b0f236df3
Sebastien Dumetz <s.dumetz@holusion.com>
2015-03-11 09:58:47 +0100

    Partial implementation of automatic camera setup

--- Assets/Scripts/HSymmetry.cs
+++ Assets/Scripts/HSymmetry.cs
@@ -1,9 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 [RequireComponent (typeof (Camera))]
 public class HSymmetry : MonoBehaviour {
+	private static string productName = null;
+	private static string productSerial = null;
+	private static Regex findCameraType = null;
+	private string type = null;
+	public HSymmetry(){
+		Regex parseHostname = new Regex(@"(prisme|focus|opera)-([0-9]*)$", RegexOptions.IgnoreCase);
+		if( productName == null ){
+			Match m = parseHostname.Match(System.Net.Dns.GetHostName());
+			if(m.Groups.Count == 3){ //Don't make defensive code here : just check for normal case
+				productName = m.Groups[1].Captures[0].Value.ToLower();
+				productSerial = m.Groups[2].Captures[0].Value.ToLower();
+			}else{
+				productName = "undefined";
+				productSerial = "undefined";
+			}
+		}
+		if(findCameraType == null){
+			findCameraType = new Regex(@"(?:camera_)(front|right|left|back)$", RegexOptions.IgnoreCase);
+		}
+	}
 
+	void Start () {
+		Match m = findCameraType.Match(this.gameObject.name);
+		if(m.Success && m.Groups.Count == 2 && m.Groups[1].Captures.Count == 1){
+			this.type = m.Groups[1].Captures[0].Value.ToLower();
+		}else{
+			this.type = "undefined";
+		}
+
+		switch (productName) {
+		case "prisme":
+
+			break;
+		case "focus":
+
+			break;
+		case "opera":
+
+			break;
+		default:
+			Debug.LogWarning("hostname invalid or indetermined. Can't setup cameras");
+			break;
+		}
+
+	}
 	void OnPreCull () {
 		Matrix4x4 scale;
 		if(camera.aspect >2){
